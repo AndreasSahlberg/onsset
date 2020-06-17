558022b433c04ecf7f8b0077460be6e49da2eb2b
Andreas Sahlberg <asahl@UG.KTH.SE>
2020-06-17 08:47:38 +0200

    Update updated_summaries.py

--- onsset/updated_summaries.py
+++ onsset/updated_summaries.py
@@ -40,8 +40,79 @@ elements = ["1.Population", "2.New_Connections", "3.Capacity", "4.Investment", "
 techs = ["Grid", "MG_Diesel", "MG_PV", "MG_Wind", "MG_Hydro", "SA_PV_1", "SA_PV_2", "SA_PV_3", "SA_PV_4", "SA_PV_5"]
 years = [2018, 2025, 2030, 2040, 2050, 2060, 2070]
 tech_codes = [1, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-tech_costs = {1: 2248, 4: 721, 5: 2950, 6: 3750, 7: 3000, 8: 9620, 9: 8780, 10: 6380, 11: 4470, 12: 6950}
 
+grid_power_plants_capital_cost = {2025: 663,
+                                  2030: 373,
+                                  2040: 3067,
+                                  2050: 982,
+                                  2060: 1583,
+                                  2070: 1352}
+
+mg_diesel_capital_cost = {2025: 721,
+                          2030: 721,
+                          2040: 721,
+                          2050: 721,
+                          2060: 721,
+                          2070: 721}
+
+mg_pv_capital_cost = {2025: 2829,
+                      2030: 2540,
+                      2040: 2252,
+                      2050: 1977,
+                      2060: 1703,
+                      2070: 1428}
+
+mg_wind_capital_cost = {2025: 4356,
+                        2030: 4285,
+                        2040: 4213,
+                        2050: 4117,
+                        2060: 4021,
+                        2070: 3926}
+
+mg_hydro_capital_cost = {2025: 3000,
+                         2030: 3000,
+                         2040: 3000,
+                         2050: 3000,
+                         2060: 3000,
+                         2070: 3000}
+
+sa_pv_capital_cost_1 = {2025: 9067,
+                        2030: 8143,
+                        2040: 7218,
+                        2050: 6338,
+                        2060: 5458,
+                        2070: 4577}
+### Stand-alone PV capital cost (USD/kW) for household systems between 21-50 W
+sa_pv_capital_cost_2 = {2025: 8487,
+                        2030: 7621,
+                        2040: 6756,
+                        2050: 5932,
+                        2060: 5108,
+                        2070: 4285}
+### Stand-alone PV capital cost (USD/kW) for household systems between 51-100 W
+sa_pv_capital_cost_3 = {2025: 6165,
+                        2030: 5537,
+                        2040: 4908,
+                        2050: 4310,
+                        2060: 3711,
+                        2070: 3113}
+### Stand-alone PV capital cost (USD/kW) for household systems between 101-1000 W
+sa_pv_capital_cost_4 = {2025: 4316,
+                        2030: 3876,
+                        2040: 3436,
+                        2050: 3017,
+                        2060: 2598,
+                        2070: 2179}
+### Stand-alone PV capital cost (USD/kW) for household systems over 1 kW
+sa_pv_capital_cost_5 = {2025: 6710,
+                        2030: 6026,
+                        2040: 5342,
+                        2050: 4690,
+                        2060: 4039,
+                        2070: 3387}
+
+tech_costs = {1: grid_power_plants_capital_cost, 4: mg_diesel_capital_cost, 5: mg_pv_capital_cost, 6: mg_wind_capital_cost, 7: mg_hydro_capital_cost,
+              8: sa_pv_capital_cost_1, 9: sa_pv_capital_cost_2, 10: sa_pv_capital_cost_3, 11: sa_pv_capital_cost_4, 12: sa_pv_capital_cost_5}
 
 for year in years:
     df_in.loc[(df_in[SET_ELEC_FINAL_CODE + '{}'.format(year)] == 3) & (df_in['PVType' + '{}'.format(year)] == 1), SET_ELEC_FINAL_CODE + '{}'.format(year)] = 8
@@ -121,11 +192,11 @@ for code in tech_codes:
         if electrified_sumtechs[year][sumtechs[i-40]] > 0:
             electrified_sumtechs[year][sumtechs[i]] = (electrified_sumtechs[year][sumtechs[i-30]]*1000000 -
                                                        electrified_sumtechs[year][sumtechs[i-10]]*1000000 -
-                                                       electrified_sumtechs[year][sumtechs[i-40]]*1000 * tech_costs[code])/(electrified_sumtechs[year][sumtechs[i-40]]*1000)
+                                                       electrified_sumtechs[year][sumtechs[i-40]]*1000 * tech_costs[code][year])/(electrified_sumtechs[year][sumtechs[i-40]]*1000)
         if unelectrified_sumtechs[year][sumtechs[i-40]] > 0:
             unelectrified_sumtechs[year][sumtechs[i]] = (unelectrified_sumtechs[year][sumtechs[i-30]]*1000000 -
                                                        unelectrified_sumtechs[year][sumtechs[i-10]]*1000000 -
-                                                       unelectrified_sumtechs[year][sumtechs[i-40]]*1000 * tech_costs[code])/(unelectrified_sumtechs[year][sumtechs[i-40]]*1000)
+                                                       unelectrified_sumtechs[year][sumtechs[i-40]]*1000 * tech_costs[code][year])/(unelectrified_sumtechs[year][sumtechs[i-40]]*1000)
     i += 1
 
 for code in tech_codes:
